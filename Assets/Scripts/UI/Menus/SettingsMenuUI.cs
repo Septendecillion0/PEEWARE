05583ff817f119067d50498bed7a4c1805d10c89
Yinuo Zhu <yinuo-zhu@outlook.com>
2025-12-03 16:02:53 -0500

    implement volume slider in game
    - can change volume with settings slider in the game scene
    - add a settings menu to the main scene, haven't add function

--- Assets/Scripts/UI/Menus/SettingsMenuUI.cs
+++ Assets/Scripts/UI/Menus/SettingsMenuUI.cs
@@ -1,12 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SettingsMenuUI : MonoBehaviour
 {
+    [Header("Volume Settings")]
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider volumeSlider;
+
+    [Header("Back Button")]
     [SerializeField] private Button backButton;
 
     private void Start()
     {
+        float v = PlayerPrefs.GetFloat("MasterVolume", 1f);
+        volumeSlider.value = v;
+        SetMasterVolume(v);
+        volumeSlider.onValueChanged.AddListener(SetMasterVolume);
+
         backButton.onClick.AddListener(SettingsManager.Instance.CloseSettings);
+    }
+
+    public void SetMasterVolume(float value)
+    {
+        float volume = Mathf.Log10(value) * 20;   // slider 0~1 → dB
+        audioMixer.SetFloat("MasterVolume", volume);
+        PlayerPrefs.SetFloat("MasterVolume", value);
     }
 }
